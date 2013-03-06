6563ceffcf0f95ceca410353f31f4844fd6a016b
Noah.Menzi <menzinoah@gmail.com>
2013-03-06 14:31:20 +0100

    added modulo()

--- collatz-vermutung.f90
+++ collatz-vermutung.f90
@@ -7,12 +7,13 @@ print *,'Bitte Zahl eingeben:'
 read *,n
 
 do while (n>1) 
-   if(( n % 2) ==0 ) then
-      n=n/2
+   if((modulo(n,2)) ==0 ) then
+      n=n/2	
       print *,n
    else 
       n=n*3-1
       print *,n
 end if
+end do
 
 end program collatz_vermutung
